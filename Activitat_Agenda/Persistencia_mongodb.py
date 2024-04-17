bf4f18fc2af8976fe423b91cf261177ea338475c
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-04-17 18:01:48 +0200

    dimecres

--- Activitat_Agenda/Persistencia_mongodb.py
+++ Activitat_Agenda/Persistencia_mongodb.py
@@ -1,29 +1,43 @@
 from typing import List
 from iAgenda_persistencia import IAgenda_persistencia
-from agenda import Agenda
-from eventos import Evento 
+from Agenda import Agenda
+from Eventos import Evento 
+import pymongo
 
 class Persistencia_agenda_mongodb(IAgenda_persistencia):
+    
+    pwd = "wv2B3otgf6QKgptC"
+    user = "2023davidhernandez"
+    cluster = "cluster0.efsedoq"
+    uri = "mongodb+srv://" + \
+            user + ":" + pwd + \
+            "@" + cluster + ".mongodb.net/?retryWrites=true&w=majority"
+    conection = pymongo.MongoClient(uri)
   
     #Da una lista de eventos a partir de su tag
     def read_evento_tag(self, tag: str) -> List[Evento]:
-        query = "db.agenda.find({ tag: '' })"
+        self.conection.evento.find_one(tag)
+        #query = "db.agenda.find({ tag: '' })"
     
     #Da una lista de todos los eventos
     def lista_eventos(self) -> List[Evento]:
-        query = "db.agenda.find()"
+        self.conection.evento.find()
+        #query = "db.agenda.find()"
 
     #Da una evento por id
     def evento_id(self, id: int) -> Evento:
-       query = "db.agenda.find({ _id: '' })"
+        self.conection.evento.find_one({"_id": id})
+        #query = "db.agenda.find({ _id: '' })"
     
     #Guarda un nuevo evento
     def save_evento(self, evento: Evento) -> Evento:
-        query = "db.agenda.insert({ _id: '', duracio: '', titulo: '', tag: '', ubicacion: '', desc: ''})"
+        self.conection.evento.insert(evento)
+        #query = "db.agenda.insert({ _id: '', duracio: '', titulo: '', tag: '', ubicacion: '', desc: ''})"
     #Actualiza un evento existente
     def actu_evento(self, id: int, evento: Evento) -> Evento:
         query = ""
     
     #Elimina un evento existente
     def delete_evento(self, id: int) -> Evento:
-        query = "db.agenda.remove({ _id: '1'})"
+        self.conection.evento.remove(id)
+        #query = "db.agenda.remove({ _id: '1'})"
