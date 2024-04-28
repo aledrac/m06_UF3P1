f8220570d0ee4a98e077a8a1144ef61d40b94438
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-04-28 18:04:05 +0200

    diumenge 5

--- Activitat_Agenda/Persistencia_mongo_Usuario.py
+++ Activitat_Agenda/Persistencia_mongo_Usuario.py
@@ -4,33 +4,37 @@ import pymongo
 from Usuario import Usuario
 
 class Persistencia_mongo_Usuario(IUsuarioPersistencia):
-    pwd = "Angel"
-    user = "Angel"
+    pwd = "david"
+    user = "david"
     cluster = "cluster0.rvcmuq8"
     uri = "mongodb+srv://" + \
             user + ":" + pwd + \
             "@" + cluster + ".mongodb.net/?retryWrites=true&w=majority"
-    conection = pymongo.MongoClient(uri)
+    def __init__(self, uri) :
+        self.conection = pymongo.MongoClient(uri)
+        self.db = self.conection.agendadb
+        self.coleccio = self.db.usuario
+    
     
     def read_user(self) -> List[Usuario]:
-        return list(self.conection.find())
+        return list(self.coleccio.find())
     
     def read_user_valor(self, valor):
-        return list(self.conection.find(valor))
+        return list(self.coleccio.find(valor))
     
     def save_user(self, user: Usuario):
         user_dict = user.to_dict()
-        ins = self.conection.insert_one(user_dict)
+        ins = self.coleccio.insert_one(user_dict)
         return ins.asknowledged()
 
     def udpate_user(self, name, user: Usuario):
         filtro = { "name": name }  
         actualizacion = { "$set": user.to_dict() }  
-        mod = self.conection.update_many(filtro, actualizacion)
+        mod = self.coleccio.update_many(filtro, actualizacion)
         return mod.modified_count()
     
     
     def delete_user(self, valor):
-        deleted = self.conection.delete_one(valor)
+        deleted = self.coleccio.delete_one(valor)
         return deleted.deleted_count()
     
