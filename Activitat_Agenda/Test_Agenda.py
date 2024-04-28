01b2eba4926bdddfe8eaa72516ec77ae15ee0b61
David Hernández Bona <2023.David.Hernandez@itibcn.cat>
2024-04-28 18:32:51 +0200

    diumenge 6

--- Activitat_Agenda/Test_Agenda.py
+++ Activitat_Agenda/Test_Agenda.py
@@ -5,31 +5,19 @@ from Persistencia_mongo_Agenda import Persistencia_mongo_Agenda
 
 class TestPersistenciaAgenda(unittest.TestCase):
     
-    def setUp(self):
-        # Configura la persistencia de agenda
-        self.persistencia_agenda = Persistencia_mongo_Agenda()
-
-        # Crea datos de prueba
-        self.agenda_prueba = Agenda(name="Mi Agenda", listaEvents=[], listaUserS=[])
-
-    def test_guardar_leer_agenda(self):
-        # Guardar agenda
-        self.assertTrue(self.persistencia_agenda.save_agenda(self.agenda_prueba))
-        
-        # Leer agenda
-        agendas = self.persistencia_agenda.read_agenda()
-        self.assertIsNotNone(agendas)
-        self.assertTrue(len(agendas) > 0)
-        
-    def test_actualizar_eliminar_agenda(self):
-        # Guardar agenda
-        self.assertTrue(self.persistencia_agenda.save_agenda(self.agenda_prueba))
-        
-        # Actualizar agenda
-        self.assertTrue(self.persistencia_agenda.update_agenda(name="Mi Agenda", agenda=self.agenda_prueba))
-        
-        # Eliminar agenda
-        self.assertTrue(self.persistencia_agenda.delete_agenda(name="Mi Agenda"))
+    def test_save_agenda(self):
+        pwd = "Angel"
+        user = "Angel"
+        cluster = "cluster0.rvcmuq8"
+        uri = "mongodb+srv://" + \
+            user + ":" + pwd + \
+            "@" + cluster + ".mongodb.net/?retryWrites=true&w=majority"
+        persistencia = Persistencia_mongo_Agenda(uri)
+        esdeveniment = Agenda("agenda1", 
+                      [["28-04-2024", "4 horas", "practica m6", "estudio", "bcn", "test para mongodb"]], 
+                      [["david", "hernandez", "david@gmail.com", "daviddavid"]])
+        inserted = persistencia.save_agenda(esdeveniment)
+        self.assertTrue(inserted)
         
 if __name__ == '__main__':
     unittest.main()
